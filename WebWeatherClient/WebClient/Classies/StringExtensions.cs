02fcfa36490ffca99fc1aa6da21a8e4463a2e6ee
Николай Полозов <kolya.polozov@inbox.ru>
2023-06-09 00:12:48 +0300

    Написал пост запросы для передачи данных между клиентом и сервером

---
+++ WebWeatherClient/WebClient/Classies/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace WebClient.Classies
+{
+    internal static class StringExtensions
+    {
+        internal static string UpperFirstChar(this string? input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+    }
+}
--- WebWeatherClient/WebClient/Controllers/HomeController.cs
+++ WebWeatherClient/WebClient/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.SignalR.Client;
 using System.Diagnostics;
 using WeatherModels;
+using WebClient.Classies;
 using WebClient.Models;
 
 namespace WebClient.Controllers
@@ -17,16 +18,55 @@ namespace WebClient.Controllers
         {
             _logger = logger;
             Configuration = configuration;
-            //Url = new Uri(Configuration["AuthorizationAdress"]);
-            //HubConnection = new HubConnectionBuilder()
-            //.WithUrl(Url)
-            //.WithAutomaticReconnect()
-            //.Build();
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+
+        public IActionResult AjaxMethodGetCity(string city)
+        {
+            if (city == null || string.IsNullOrWhiteSpace(city))
+                return new BadRequestResult();
+            string City = city.ToLower().UpperFirstChar();
+            string? whatCity = CitiesClass.cities.Split("\r\n").Where(c => c.Contains(City)).FirstOrDefault();
+            if(whatCity == null)
+                return new BadRequestResult();
+            string[] whatLatLon = whatCity.Split(' ');
+            string lattitude = whatLatLon[2].Remove(whatLatLon[2].Length - 1);
+            string longitude = whatLatLon[3];
+            var data = new
+            {
+                сity = City,
+                lattitude = lattitude,
+                longitude = longitude,
+            };
+            return Json(data);
+        }
+
+        [HttpPost]
+        public IActionResult AjaxMethodGetWeather(string a)
+        {
+            //foreach(Weather weather in listWeathers)
+            //{
+            //    Debug.WriteLine(weather.Id);
+            //    Debug.WriteLine(weather.City);
+            //    Debug.WriteLine(weather.Lattitude);
+            //    Debug.WriteLine(weather.Longitude);
+            //    Debug.WriteLine(weather.Date);
+            //    Debug.WriteLine(weather.MaxTemperature);
+            //    Debug.WriteLine(weather.MinTemperature);
+            //    Debug.WriteLine(weather.WeatherImageSource);
+            //    Debug.WriteLine(weather.WeatherDescription);
+            //    Debug.WriteLine(weather.Pressure);
+            //    Debug.WriteLine(weather.Humidity);
+            //    Debug.WriteLine(weather.WindSpeed);
+            //    Debug.WriteLine(weather.WindDir);
+            //    Debug.WriteLine(weather.FeelsLike);
+            //}
+            return Json("");
         }
 
         public IActionResult Privacy()
